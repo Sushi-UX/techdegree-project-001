85d368aef7278298be97c3267b8ddab74c5badb1
Sushi-UX <thechoygroup@gmail.com>
2019-05-23 18:56:30 -0400

    Dictionary of players
    Manually entered players as dictionary

---
+++ Soccer-Coordinator-Playground.playground/Contents.swift
@@ -0,0 +1,100 @@
+// Treehouse Tech degree - Unit 001 - Soccer Coordinator
+
+//Declare dictionary of all players
+var player1 = ["name": "Joe Smith",
+               "height": "42.00",
+               "experience": "yes",
+               "guardian": "Jim and Jan Smith"]
+
+var player2 = ["name": "Jill Tanner",
+               "height": "36.00",
+               "experience": "yes",
+               "guardian": "Clara Tanner"]
+
+var player3 = ["name": "Bill Bon",
+               "height": "43.00",
+               "experience": "yes",
+               "guardian": "Sara and Jenny Bon"]
+
+var player4 = ["name": "Eva Gordon",
+               "height": "45.00",
+               "experience": "no",
+               "guardian": "Wendy and Mike Gordon"]
+
+var player5 = ["name": "Matt Gill",
+               "height": "40.00",
+               "experience": "no",
+               "guardian": "Charles and Sylvia Gill"]
+
+var player6 = ["name": "Kimmy Stein",
+               "height": "41.00",
+               "experience": "no",
+               "guardian": "Bill and Hillary Stein"]
+
+var player7 = ["name": "Sammy Adams",
+               "height": "45.00",
+               "experience": "no",
+               "guardian": "Jeff Adams"]
+
+var player8 = ["name": "Karl Saygan",
+               "height": "42.00",
+               "experience": "yes",
+               "guardian": "Heather Bledsoe"]
+
+var player9 = ["name": "Suzane Greenberg",
+               "height": "44.00",
+               "experience": "yes",
+               "guardian": "Henrietta Dumas"]
+
+var player10 = ["name": "Sal Dali",
+               "height": "41.00",
+               "experience": "no",
+               "guardian": "Gala Dali"]
+
+var player11 = ["name": "Joe Kavalier",
+               "height": "39.00",
+               "experience": "no",
+               "guardian": "Sam and Elaine Kavalier"]
+
+var player12 = ["name": "Ben Finkelstein",
+               "height": "44.00",
+               "experience": "no",
+               "guardian": "Aaron and Jill Finkelstein"]
+
+var player13 = ["name": "Diego Soto",
+               "height": "41.00",
+               "experience": "yes",
+               "guardian": "Robin and Sarika Soto"]
+
+var player14 = ["name": "Chloe Alaska",
+               "height": "47.00",
+               "experience": "no",
+               "guardian": "David and Jamie Alaska"]
+
+var player15 = ["name": "Arnold Willis",
+               "height": "43.00",
+               "experience": "no",
+               "guardian": "Claire Willis"]
+
+var player16 = ["name": "Phillip Helm",
+               "height": "44.00",
+               "experience": "yes",
+               "guardian": "Thomas Helm and Eva Jones"]
+
+var player17 = ["name": "Les Clay",
+               "height": "42.00",
+               "experience": "yes",
+               "guardian": "Wynonna Brown"]
+
+var player18 = ["name": "Herschel Krustofski",
+               "height": "45.00",
+               "experience": "yes",
+               "guardian": "Hyman and Rachel Krustofski"]
+
+
+
+
+
+
+
+
